8a3c6d006064f2648a456250ecc1c263d5db8b5a
Wolfdog <vuckoo@hotmail.com>
2018-11-12 22:43:27 +0100

    bb

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,14 @@
-$
+$(document).ready(function () {
+
+
+
+    $('#youtube').on('submit', (e) => {
+        e.preventDefault();
+        const id = $('#youtube-id').val();
+        
+        
+
+        window.open(`/youtube/${id}`, '_blank')
+    })
+
+})
--- routes/youtube.js
+++ routes/youtube.js
@@ -5,18 +5,19 @@ const { join } = require('path');
 const output = join(__dirname, '../public/uploads/');
 const fs = require('fs');
 
-router.get('/youtube', async (req, res) => {
-
+router.get('/', async (req, res) => {
+    res.render('youtube')
 })
 
-router.get('/test/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
     const id = req.params.id;
+    if(!/^[a-zA-Z0-9-_]{11}$/.test(id)) return res.status(400).send('Invalid ID')
     const yt = `https://www.youtube.com/watch?v=${id}`
     const name = `${id}.mp3`
     const cmdFileName = `youtube-dl --get-title ${yt}`
     const cmd = `youtube-dl --extract-audio --audio-format mp3 --output ${output + id}.%(ext)s ${yt}`
     exec(cmdFileName, (error, stdout, stderr) => {
-        if (error) return res.render('error', { error, message: 'Greska' })
+        if (error) return res.status(400).json({ error, message: 'Greska' })
         const fileName = stdout.slice(0, stdout.length - 1) + '.mp3';
         exec(cmd, (err, stdout, stderr) => {
 
