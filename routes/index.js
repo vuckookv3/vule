a98f24586449371807f84ac67dc0ac756fe6a1de
Wolfdog <vuckoo@hotmail.com>
2018-11-12 21:29:37 +0100

    vule zavrsio yt mp3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('login');
-});
+router.use('/youtube', require('./youtube'));
 
 module.exports = router;
