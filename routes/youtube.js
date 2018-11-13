55329e32bc523e2ae29f460288f021c8e56bb6eb
Marko Vucetic <marko.vucetic@riitech.com>
2018-11-13 11:19:48 +0100

    sredjena komanda i za linux

--- routes/youtube.js
+++ routes/youtube.js
@@ -11,16 +11,19 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     const id = req.params.id;
-    if(!/^[a-zA-Z0-9-_]{11}$/.test(id)) return res.status(400).send('Invalid ID')
+    if (!/^[a-zA-Z0-9-_]{11}$/.test(id)) return res.status(400).send('Invalid ID')
     const yt = `https://www.youtube.com/watch?v=${id}`
     const name = `${id}.mp3`
     const cmdFileName = `youtube-dl --get-title ${yt}`
-    const cmd = `youtube-dl --extract-audio --audio-format mp3 --output ${output + id}.%(ext)s ${yt}`
+    const cmd = `youtube-dl --extract-audio --audio-format mp3 --output "${output + id}.%(ext)s" ${yt}`
     exec(cmdFileName, (error, stdout, stderr) => {
         if (error) return res.status(400).json({ error, message: 'Greska' })
         const fileName = stdout.slice(0, stdout.length - 1) + '.mp3';
         exec(cmd, (err, stdout, stderr) => {
-
+            if (err) {
+                console.error(err)
+                return res.status(400).json(err)
+            }
             res.download(`${output + name}`, `${fileName}`)
             res.on('finish', () => {
                 fs.unlink(`${output + name}`, (err) => {
